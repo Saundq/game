580ef644ef8f5c789aa5b77e633ab69b9ea90df2
Alexandr R. aka Saund <saund007@gmail.com>
2023-11-07 10:21:49 +0300

    initial commit

---
+++ pkg/life/life.go
@@ -0,0 +1,96 @@
+package life
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+type World struct {
+	Height int // Высота сетки
+	Width  int // Ширина сетки
+	Cells  [][]bool
+}
+
+// Используйте код из предыдущего урока по игре «Жизнь»
+func NewWorld(height, width int) (World, error) {
+	// создаём тип World с количеством слайсов hight (количество строк)
+	cells := make([][]bool, height)
+	for i := range cells {
+		cells[i] = make([]bool, width) // создаём новый слайс в каждой строке
+	}
+	if height < 0 || width < 0 {
+		return &World{}, errors.New("Размерность не может быть отрицательной")
+	}
+	return World{
+		Height: height,
+		Width:  width,
+		Cells:  cells,
+	}, nil
+}
+func (w *World) next(x, y int) bool {
+	n := w.neighbors(x, y)       // получим количество живых соседей
+	alive := w.Cells[y][x]       // текущее состояние клетки
+	if n < 4 && n > 1 && alive { // если соседей двое или трое, а клетка жива
+		return true // то следующее состояние — жива
+	}
+	if n == 3 && !alive { // если клетка мертва, но у неё трое соседей
+		return true // клетка оживает
+	}
+
+	return false // в любых других случаях — клетка мертва
+}
+func (w *World) neighbors(x, y int) int {
+	n := 0
+	for v := -1; v <= 1; v++ {
+		for h := -1; h <= 1; h++ {
+			if !(v == 0 && h == 0) && w.Cells[y][x] { //u.Alive(x+h, y+v) {
+				n++
+			}
+		}
+	}
+	return n
+}
+func NextState(oldWorld, newWorld World) {
+	// переберём все клетки, чтобы понять, в каком они состоянии
+	for i := 0; i < oldWorld.Height; i++ {
+		for j := 0; j < oldWorld.Width; j++ {
+			// для каждой клетки получим новое состояние
+			newWorld.Cells[i][j] = oldWorld.next(j, i)
+		}
+	}
+}
+
+// RandInit заполняет поля на указанное число процентов
+func (w *World) RandInit(percentage int) {
+	// Количество живых клеток
+	numAlive := percentage * w.Height * w.Width / 100
+	// Заполним живыми первые клетки
+	w.fillAlive(numAlive)
+	// Получаем рандомные числа
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	// Рандомно меняем местами
+	for i := 0; i < w.Height*w.Width; i++ {
+		randRowLeft := r.Intn(w.Width)
+		randColLeft := r.Intn(w.Height)
+		randRowRight := r.Intn(w.Width)
+		randColRight := r.Intn(w.Height)
+
+		w.Cells[randRowLeft][randColLeft] = w.Cells[randRowRight][randColRight]
+	}
+}
+
+func (w *World) fillAlive(num int) {
+	aliveCount := 0
+	for j, row := range w.Cells {
+		for k := range row {
+			w.Cells[j][k] = true
+			aliveCount++
+			if aliveCount == num {
+
+				return
+			}
+		}
+	}
+}
